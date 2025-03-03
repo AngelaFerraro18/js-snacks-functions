2e76a436d8a29b9a479fee1541809d51d81b853d
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-03 14:45:32 +0100

    feat: 2nd exercise completed

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,29 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function namesInitials (list1){
+    let namesFistLetter = [];
+    for ( let i = 0; i < names.length; i++){
+        namesItem = names[i];
+        namesFistLetter.push(namesItem.at(0));
+    
+    } return namesFistLetter;
+}
+
+/* soluzione arrow function
+const namesInitials = list1 => {
+    let namesFistLetter = [];
+    for ( let i = 0; i < names.length; i++){
+        namesItem = names[i];
+        namesFistLetter.push(namesItem.at(0));
+    
+    } return namesFistLetter;
+}*/
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(namesInitials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
