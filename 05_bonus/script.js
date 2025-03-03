e52438e79797b988366090241200950f6dde7ce1
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-03 17:49:22 +0100

    feat 5th exercise completed

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,45 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const nameUser = 'Mario';
 
 // Dichiara la funzione qui.
 
+function helloUser(userName) {
+    const today = new Date();
+    let saluto;
+
+    if (today < 13) {
+        saluto = `Buongiorno ${nameUser}`;
+    } else if (today < 17) {
+        saluto = `Buon pomeriggio ${nameUser}`;
+    } else {
+        saluto = `Buonasera ${nameUser}`;
+    }
+    return saluto;
+}
+
+/* soluzione arrow function
+const helloUser = userName =>{
+    const today = new Date();
+    let saluto;
+
+    if (today < 13){
+        saluto = `Buongiorno ${nameUser}`;
+    } else if (today < 17){
+        saluto = `Buon pomeriggio ${nameUser}`;
+    } else {
+        saluto = `Buonasera ${nameUser}`;
+    }
+    return saluto;
+}
+*/
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(helloUser(nameUser));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+
+
