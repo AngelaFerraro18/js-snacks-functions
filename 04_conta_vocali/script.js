09d9f70d4ec4c76878f3c9f8a27a601ca0344040
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-03 16:56:04 +0100

    feat: 4th exercise completed

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,34 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+let count = 0;
+const vocals = ['a', 'e', 'i', 'o', 'u'];
+
+function countVocals(wordInput) {
+    for (let i = 0; i < word.length; i++) {
+        for (let j = 0; j < vocals.length; j++) {
+            if (word[i] === vocals[j]) {
+                count++;
+            }
+        }
+    } return count;
+}
+
+/* soluzione arrow function
+
+const countVocals = wordInput =>{
+      for (let i = 0; i < word.length; i++) {
+            for (let j = 0; j < vocals.length; j++) {
+                if (word[i] === vocals[j]) {
+                    count++;
+                }
+            }
+        } return count;
+     }
+    */
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(countVocals(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
